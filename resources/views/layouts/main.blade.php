698b3caa27e8b352b449661832c802dd22d9ca5d
Irene Simatupang <irenesimatupang07@gmail.com>
2024-04-16 11:36:33 +0700

    Dashboard

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <!-- Bootstrap CSS -->
+
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+    <title>Hello, world!</title>
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
+  </head>
+  <body class="sb-nav-fixed">
+    @yield('content')
+
+
+    <script src="{{asset('js/datatables-simple-demo.js')}}"></script>
+    <script src="{{asset('js/scripts.js')}}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="{{asset('assets/demo/chart-area-demo.js')}}"></script>
+    <script src="{{asset('assets/demo/chart-bar-demo.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+  </body>
+</html>
